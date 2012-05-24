979a6f63dae6b169620486502c75deaecc84d7a4
chorn <chorn@chorn.com>
2012-05-24 10:08:36 -0400

    Added more attributes to SurveyCampaign

--- lib/survey_gizmo/api/survey_campaign.rb
+++ lib/survey_gizmo/api/survey_campaign.rb
@@ -5,22 +5,29 @@ module SurveyGizmo; module API
 
     # @macro [attach] virtus_attribute
     #   @return [$2]
-    attribute :id,            Integer
-    attribute :name,          String
-    attribute :type,          String
-    attribute :status,        String
-    attribute :slug,          String
-    attribute :language,      String
-    attribute :survey_id,     Integer
-
-    alias_attribute :_subtype, :type
+    attribute :id,              Integer
+    attribute :name,            String
+    attribute :type,            String
+    attribute :_subtype,        String
+    attribute :__subtype,       String
+    attribute :status,          String
+    attribute :uri,             String
+    attribute :SSL,             Boolean
+    attribute :slug,            String
+    attribute :language,        String
+    attribute :close_message,   String
+    attribute :limit_responses, String
+    # attribute :tokenvariables,  String
+    attribute :survey_id,       Integer
+    attribute :datecreated,     DateTime
+    attribute :datemodified,    DateTime
 
     route '/survey/:survey_id/surveycampaign', :via => :get
     route '/survey/:survey_id/surveycampaign', :via => :create
     route '/survey/:survey_id/surveycampaign/:id', :via => [:update, :delete]
 
     # @macro collection
-    collection :options
+    collection :tokenvariables
 
     # @see SurveyGizmo::Resource#to_param_options
     def to_param_options
