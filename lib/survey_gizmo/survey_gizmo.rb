fd8f7106bfd7653cdee58c971ee0039c4f83b77c
apurvis@lumoslabs.com <apurvis@lumoslabs.com>
2015-03-10 19:00:28 -0700

    Whitespace

--- lib/survey_gizmo/survey_gizmo.rb
+++ lib/survey_gizmo/survey_gizmo.rb
@@ -1,23 +1,23 @@
-require "active_support/core_ext/string"
-require "active_support/core_ext/module"
-require "active_support/core_ext/hash"
-require "active_support/core_ext/object/blank"
-require "active_support/concern"
-require "virtus"
-require "httparty"
-require "digest/md5"
+require 'active_support/core_ext/string'
+require 'active_support/core_ext/module'
+require 'active_support/core_ext/hash'
+require 'active_support/core_ext/object/blank'
+require 'active_support/concern'
+require 'virtus'
+require 'httparty'
+require 'digest/md5'
 
-require "survey_gizmo/resource"
-require "survey_gizmo/collection"
+require 'survey_gizmo/resource'
+require 'survey_gizmo/collection'
 
-require "survey_gizmo/api/survey"
-require "survey_gizmo/api/survey_campaign"
-require "survey_gizmo/api/question"
-require "survey_gizmo/api/option"
-require "survey_gizmo/api/page"
-require "survey_gizmo/api/contact"
-require "survey_gizmo/api/response"
-require "survey_gizmo/api/email_message"
+require 'survey_gizmo/api/survey'
+require 'survey_gizmo/api/survey_campaign'
+require 'survey_gizmo/api/question'
+require 'survey_gizmo/api/option'
+require 'survey_gizmo/api/page'
+require 'survey_gizmo/api/contact'
+require 'survey_gizmo/api/response'
+require 'survey_gizmo/api/email_message'
 
 module SurveyGizmo
   include HTTParty
