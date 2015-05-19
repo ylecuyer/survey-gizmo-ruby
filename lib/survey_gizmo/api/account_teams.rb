7aa6b627f2009be27bf65e74a280d213d512db91
apurvis@lumoslabs.com <apurvis@lumoslabs.com>
2015-05-19 02:54:49 -0700

    Account teams documentation

--- lib/survey_gizmo/api/account_teams.rb
+++ lib/survey_gizmo/api/account_teams.rb
@@ -1,17 +1,16 @@
 module SurveyGizmo;
   module API
-  # @see SurveyGizmo::Resource::ClassMethods
     class AccountTeams
       include SurveyGizmo::Resource
 
       # @macro [attach] virtus_attribute
       #   @return [$2]
-      attribute :id, Integer
+      attribute :id,            Integer
       attribute :teamid,        Integer
       attribute :teamname,      String
-      attribute :color,          String
-      attribute :default_role,   String
-      attribute :status,         String
+      attribute :color,         String
+      attribute :default_role,  String
+      attribute :status,        String
 
       route '/accountteams/:id', via: [:get, :update, :delete]
       route '/accountteams',     via: :create
