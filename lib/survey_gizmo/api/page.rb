dcf1a20878c2e16af32c8ab28c8c122efa2df93a
apurvis@lumoslabs.com <apurvis@lumoslabs.com>
2015-10-17 21:57:02 -0400

    Refactor the multilingual title parsing to a new module

--- lib/survey_gizmo/api/page.rb
+++ lib/survey_gizmo/api/page.rb
@@ -4,15 +4,17 @@ module SurveyGizmo; module API
     include SurveyGizmo::Resource
 
     attribute :id,            Integer
-    attribute :title,         Hash
+    attribute :title,         String
     attribute :description,   String
     attribute :properties,    Hash
     attribute :after,         Integer
     attribute :survey_id,     Integer
 
+    include SurveyGizmo::MultilingualTitle
+
     # routing
-    route '/survey/:survey_id/surveypage', :via => :create
-    route '/survey/:survey_id/surveypage/:id', :via => [:get, :update, :delete]
+    route '/survey/:survey_id/surveypage', via: :create
+    route '/survey/:survey_id/surveypage/:id', via: [:get, :update, :delete]
 
     def survey
       @survey ||= SurveyGizmo::API::Survey.first(id: survey_id)
@@ -21,13 +23,6 @@ module SurveyGizmo; module API
     def questions
       @questions ||= SurveyGizmo::API::Question.all(survey_id: survey_id, page_id: id)
     end
-
-    # survey gizmo sends a hash back for :title
-    def title_with_multilingual=(val)
-      self.title_without_multilingual = val.is_a?(Hash) ? val : { 'English' => val }
-    end
-
-    alias_method_chain :title=, :multilingual
 
     def to_param_options
       { id: self.id, survey_id: self.survey_id }
