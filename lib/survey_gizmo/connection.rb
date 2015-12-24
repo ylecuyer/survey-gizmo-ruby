ee51889fa6134e97be24f2892094c88c9d6a5fef
apurvis@lumoslabs.com <apurvis@lumoslabs.com>
2015-12-24 11:18:50 -0500

    Pester middleware

--- lib/survey_gizmo/connection.rb
+++ lib/survey_gizmo/connection.rb
@@ -1,6 +1,17 @@
 module SurveyGizmo
   class Connection
     include Singleton
+    extend Forwardable
+
+    def_delegators :connection, :get, :put, :delete
+
+    class PesterMiddleware < Faraday::Middleware
+      Faraday::Response.register_middleware(pester: self)
+
+      def call(environment)
+        Pester.survey_gizmo_ruby.retry { @app.call(environment) }
+      end
+    end
 
     def reset!
       @connection = nil
@@ -12,18 +23,6 @@ module SurveyGizmo
           request.body = { data: payload }.to_json
         end
       end
-    end
-
-    def get(route, query = {})
-      Pester.survey_gizmo_ruby.retry { connection.get(route, query.merge(authentication_params)) }
-    end
-
-    def put(route, query = {})
-      Pester.survey_gizmo_ruby.retry { connection.put(route, query.merge(authentication_params)) }
-    end
-
-    def delete(route)
-      Pester.survey_gizmo_ruby.retry { connection.delete(route, authentication_params) }
     end
 
     def api_debug?
@@ -38,18 +37,16 @@ module SurveyGizmo
     def connection
       fail 'Not configured' unless SurveyGizmo.configuration
 
-      @connection ||= Faraday.new("https://restapi.surveygizmo.com") do |connection|
+      auth_params = { 'user:md5' => "#{SurveyGizmo.configuration.user}:#{Digest::MD5.hexdigest(SurveyGizmo.configuration.password)}" }
+      @connection ||= Faraday.new(url: "https://restapi.surveygizmo.com", params: auth_params) do |connection|
         connection.request :url_encoded
 
+        connection.response :pester
         connection.response :logger, @logger, bodies: true if api_debug?
         connection.response :json, content_type: /\bjson$/
 
         connection.adapter Faraday.default_adapter
       end
-    end
-
-    def authentication_params
-      { 'user:md5' => "#{SurveyGizmo.configuration.user}:#{Digest::MD5.hexdigest(SurveyGizmo.configuration.password)}" }
     end
   end
 end
