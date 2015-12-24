18b35485553814947ed557995355cf7da774bceb
apurvis@lumoslabs.com <apurvis@lumoslabs.com>
2015-12-24 16:22:05 -0500

    Simplify file loading

--- lib/survey-gizmo-ruby.rb
+++ lib/survey-gizmo-ruby.rb
@@ -1,6 +1,5 @@
 require 'active_support/concern'
 require 'active_support/core_ext/array'
-require 'active_support/core_ext/object/blank'
 require 'active_support/core_ext/hash'
 require 'active_support/core_ext/module'
 require 'active_support/core_ext/object/blank'
@@ -15,22 +14,7 @@ require 'logger'
 require 'pester'
 require 'virtus'
 
-require 'survey_gizmo/version'
-require 'survey_gizmo/configuration'
-require 'survey_gizmo/connection'
-require 'survey_gizmo/multilingual_title'
-require 'survey_gizmo/resource'
+path = File.join(File.expand_path(File.dirname(__FILE__)), 'survey_gizmo')
 
-require 'survey_gizmo/faraday_middleware/parse_survey_gizmo'
-require 'survey_gizmo/faraday_middleware/pester_survey_gizmo'
-
-require 'survey_gizmo/api/account_teams'
-require 'survey_gizmo/api/answer'
-require 'survey_gizmo/api/contact'
-require 'survey_gizmo/api/email_message'
-require 'survey_gizmo/api/option'
-require 'survey_gizmo/api/page'
-require 'survey_gizmo/api/question'
-require 'survey_gizmo/api/response'
-require 'survey_gizmo/api/survey'
-require 'survey_gizmo/api/survey_campaign'
+Dir.glob("#{path}/*.rb").each { |f| require f.sub(/\.rb$/, '') }
+Dir.glob("#{path}/**/*.rb").each { |f| require f.sub(/\.rb$/, '') }
