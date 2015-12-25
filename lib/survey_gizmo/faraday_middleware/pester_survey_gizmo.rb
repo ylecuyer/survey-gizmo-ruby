75fbb288a98c7580fbb3ac3528f88465583b6f02
apurvis@lumoslabs.com <apurvis@lumoslabs.com>
2015-12-24 21:12:12 -0500

    Only need the error message in this case

--- lib/survey_gizmo/faraday_middleware/pester_survey_gizmo.rb
+++ lib/survey_gizmo/faraday_middleware/pester_survey_gizmo.rb
@@ -7,7 +7,7 @@ module SurveyGizmo
         @app.call(environment).on_complete do |response|
           fail RateLimitExceededError if response.status == 429
           fail BadResponseError, "Bad response code #{response.status} in #{response.inspect}" unless response.status == 200
-          fail BadResponseError, response.inspect unless response.body['result_ok'] && response.body['result_ok'].to_s.downcase == 'true'
+          fail BadResponseError, response.body['message'] unless response.body['result_ok'] && response.body['result_ok'].to_s.downcase == 'true'
         end
       end
     end
