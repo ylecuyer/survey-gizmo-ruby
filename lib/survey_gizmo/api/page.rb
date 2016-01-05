4f39d28a3327ce1bb75f5c72291dc44ac903660f
apurvis@lumoslabs.com <apurvis@lumoslabs.com>
2016-01-04 19:28:35 -0500

    Fix the passing down of parent attributes to children when parsing elements from surveygizmo JSON

--- lib/survey_gizmo/api/page.rb
+++ lib/survey_gizmo/api/page.rb
@@ -20,14 +20,17 @@ module SurveyGizmo; module API
     end
 
     def questions
-      # See note about broken subquestions in resource.rb
-      @questions.flat_map { |q| q.sub_question_skus }.each do |sku|
-        sku = sku[1] if sku.is_a?(Array)
-        next if @questions.find { |q| q.id == sku }
-        @questions << Question.first(children_params.merge(id: sku))
+      @questions.each { |q| q.attributes = children_params }
+      return @questions if @questions.all? { |q| q.sub_question_skus.all? { |sku| @questions.find { |q| q.id == sku } } }
+
+      # See note on broken subquestions in resource.rb.
+      with_subquestions = @questions
+      @questions.each do |q|
+        with_subquestions.reject! { |q| q.sub_question_skus.include?(q.id) }
+        with_subquestions += q.sub_questions
       end
 
-      @questions.each { |q| q.attributes = children_params }
+      @questions = with_subquestions.each { |q| q.attributes = children_params }
     end
   end
 end; end
