1650b5498026f0ab472d8eb6c1376fbe0488b974
Martin Krueger <martin@playtestcloud.com>
2016-12-08 22:54:45 +0100

    Bugfix: mask cgi escaped (percent encoded) api tokens in logs

--- lib/survey_gizmo/logger.rb
+++ lib/survey_gizmo/logger.rb
@@ -2,11 +2,30 @@ require 'logger'
 
 module SurveyGizmo
   class Logger < ::Logger
-    def format_message(severity, timestamp, progname, msg)
-      msg.gsub!(/#{Regexp.quote(SurveyGizmo.configuration.api_token)}/, '<SG_API_KEY>') if SurveyGizmo.configuration.api_token
-      msg.gsub!(/#{Regexp.quote(SurveyGizmo.configuration.api_token_secret)}/, '<SG_API_SECRET>') if SurveyGizmo.configuration.api_token_secret
+    def format_message(severity, timestamp, progname, message)
+      api_token = SurveyGizmo.configuration.api_token
+      api_token_secret = SurveyGizmo.configuration.api_token_secret
 
-      "#{timestamp.strftime('%Y-%m-%d %H:%M:%S')} #{severity} #{msg}\n"
+      message.gsub!(
+        /#{Regexp.quote(api_token)}/,
+        '<SG_API_KEY>'
+      )
+      message.gsub!(
+        /#{Regexp.quote(api_token_secret)}/,
+        '<SG_API_SECRET>'
+      )
+
+      # in case the tokens are percent encoded according to CGI query spec
+      message.gsub!(
+        /#{Regexp.quote(CGI.escape(api_token))}/,
+        '<SG_API_KEY>'
+      )
+      message.gsub!(
+        /#{Regexp.quote(CGI.escape(api_token_secret))}/,
+        '<SG_API_SECRET>'
+      )
+
+      "#{timestamp.strftime('%Y-%m-%d %H:%M:%S')} #{severity} #{message}\n"
     end
   end
 end
