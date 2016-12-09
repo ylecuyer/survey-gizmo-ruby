5a2e9f1e364d80e27bc719b2fea60416b2b2190b
Martin Krueger <martin@playtestcloud.com>
2016-12-09 11:56:18 +0100

    Refactor[PR-#91]: use safe assignment idiom

--- lib/survey_gizmo/logger.rb
+++ lib/survey_gizmo/logger.rb
@@ -3,18 +3,19 @@ require 'logger'
 module SurveyGizmo
   class Logger < ::Logger
     def format_message(severity, timestamp, progname, message)
-      api_token = SurveyGizmo.configuration.api_token
-      api_token_secret = SurveyGizmo.configuration.api_token_secret
+      if (api_token = SurveyGizmo.configuration.api_token)
+        message.gsub!(
+          /#{Regexp.quote(api_token)}|#{Regexp.quote(CGI.escape(api_token))}/,
+          '<SG_API_KEY>'
+        )
+      end
 
-      message.gsub!(
-        /#{Regexp.quote(api_token)}|#{Regexp.quote(CGI.escape(api_token))}/,
-        '<SG_API_KEY>'
-      ) if api_token
-
-      message.gsub!(
-        /#{Regexp.quote(api_token_secret)}|#{Regexp.quote(CGI.escape(api_token_secret))}/,
-        '<SG_API_SECRET>'
-      ) if api_token_secret
+      if (api_token_secret = SurveyGizmo.configuration.api_token_secret)
+        message.gsub!(
+          /#{Regexp.quote(api_token_secret)}|#{Regexp.quote(CGI.escape(api_token_secret))}/,
+          '<SG_API_SECRET>'
+        )
+      end
 
       "#{timestamp.strftime('%Y-%m-%d %H:%M:%S')} #{severity} #{message}\n"
     end
