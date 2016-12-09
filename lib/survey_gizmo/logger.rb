ed8cfe165a1dc6f3998a7dd65f08fb6d3dfc1140
Martin Krueger <martin@playtestcloud.com>
2016-12-09 09:01:20 +0100

    Refactor[PR-#91]: adding OR to regex instead of repeating gsub statements

--- lib/survey_gizmo/logger.rb
+++ lib/survey_gizmo/logger.rb
@@ -7,23 +7,12 @@ module SurveyGizmo
       api_token_secret = SurveyGizmo.configuration.api_token_secret
 
       message.gsub!(
-        /#{Regexp.quote(api_token)}/,
+        /#{Regexp.quote(api_token)}|#{Regexp.quote(CGI.escape(api_token))}/,
         '<SG_API_KEY>'
       ) if api_token
 
       message.gsub!(
-        /#{Regexp.quote(api_token_secret)}/,
-        '<SG_API_SECRET>'
-      ) if api_token_secret
-
-      # in case the tokens are percent encoded according to CGI query spec
-      message.gsub!(
-        /#{Regexp.quote(CGI.escape(api_token))}/,
-        '<SG_API_KEY>'
-      ) if api_token
-
-      message.gsub!(
-        /#{Regexp.quote(CGI.escape(api_token_secret))}/,
+        /#{Regexp.quote(api_token_secret)}|#{Regexp.quote(CGI.escape(api_token_secret))}/,
         '<SG_API_SECRET>'
       ) if api_token_secret
 
