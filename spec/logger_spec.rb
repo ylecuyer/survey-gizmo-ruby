1a4b9c0fc75c9798c12a95e9329607ec58e40161
apurvis@lumoslabs.com <apurvis@lumoslabs.com>
2016-12-14 14:56:37 -0500

    RSpec-ification cc @mkaydev this is how you rspec

--- spec/logger_spec.rb
+++ spec/logger_spec.rb
@@ -1,14 +1,15 @@
 require 'spec_helper'
 
-describe SurveyGizmo::Configuration do
+describe SurveyGizmo::Logger do
+  let(:progname)    { 'TEST' }
+  let(:severity)    { 'INFO' }
+  let(:time_string) { '2015-04-15 05:46:30' }
+
   before(:each) do
     SurveyGizmo.configure do |config|
       config.api_token = 'king_of_the&whirled$'
       config.api_token_secret = 'dream/word'
     end
-    @severity = 'INFO'
-    @time_string = '2015-04-15 05:46:30'
-    @progname = 'TEST'
   end
 
   after(:each) do
@@ -18,60 +19,60 @@ describe SurveyGizmo::Configuration do
   it 'should mask unencoded api token' do
     config = SurveyGizmo.configuration
     formatted_message = config.logger.format_message(
-      @severity,
-      @time_string.to_time,
-      @progname,
+      severity,
+      time_string.to_time,
+      progname,
       config.api_token
     )
     expect(
       formatted_message
-    ).to eq(
-      "#{@time_string} #{@severity} <SG_API_KEY>\n"
+    ).to match(
+      /\[#{time_string} #{severity} \(\d+\)\] <SG_API_KEY>/
     )
   end
 
   it 'should mask percent encoded api token' do
     config = SurveyGizmo.configuration
     formatted_message = config.logger.format_message(
-      @severity,
-      @time_string.to_time,
-      @progname,
+      severity,
+      time_string.to_time,
+      progname,
       CGI.escape(config.api_token)
     )
     expect(
       formatted_message
-    ).to eq(
-      "#{@time_string} #{@severity} <SG_API_KEY>\n"
+    ).to match(
+      /\[#{time_string} #{severity} \(\d+\)\] <SG_API_KEY>/
     )
   end
 
   it 'should mask unencoded api token secret' do
     config = SurveyGizmo.configuration
     formatted_message = config.logger.format_message(
-      @severity,
-      @time_string.to_time,
-      @progname,
+      severity,
+      time_string.to_time,
+      progname,
       config.api_token_secret
     )
     expect(
       formatted_message
-    ).to eq(
-      "#{@time_string} #{@severity} <SG_API_SECRET>\n"
+    ).to match(
+      /\[#{time_string} #{severity} \(\d+\)\] <SG_API_SECRET>/
     )
   end
 
   it 'should mask percent encoded api token secret' do
     config = SurveyGizmo.configuration
     formatted_message = config.logger.format_message(
-      @severity,
-      @time_string.to_time,
-      @progname,
+      severity,
+      time_string.to_time,
+      progname,
       CGI.escape(config.api_token_secret)
     )
     expect(
       formatted_message
-    ).to eq(
-      "#{@time_string} #{@severity} <SG_API_SECRET>\n"
+    ).to match(
+      /\[#{time_string} #{severity} \(\d+\)\] <SG_API_SECRET>/
     )
   end
 end
